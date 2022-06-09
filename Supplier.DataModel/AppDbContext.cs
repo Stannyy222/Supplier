3230af3c3de853ca6879b0b7739d3d06006ddadc
HuertoStan <12108906@benilde.edu.ph>
2022-06-09 16:56:58 +0800

    Suppliers AppDbContext

---
+++ Supplier.DataModel/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supplier.DataModel
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-LS461TD\\SQLEXPRESS; " +
+                "Database=Entprog_Supplier; Integrated Security=SSPI; " +
+                "TrustServerCertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<supplier>().Property(p => p.CompanyName).HasColumnType("nvarchar(100)");
+            modelBuilder.Entity<supplier>().Property(p => p.Address).HasColumnType("nvarchar(200)");
+            modelBuilder.Entity<supplier>().Property(p => p.Representative).HasColumnType("nvarchar(100)");
+            modelBuilder.Entity<supplier>().Property(p => p.ContactNo).HasColumnType("nvarchar(50)");
+            modelBuilder.Entity<supplier>().Property(p => p.DateModified).HasColumnType("datetime");
+
+        }            
+        
+        DbSet<supplier> SuppliersINV { get; set; }
+    }
+}
---
+++ Supplier.DataModel/supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supplier.DataModel
+{
+    public class supplier
+    {
+        public int SupplierID { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string Representative { get; set; }
+        public string ContactNo { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
